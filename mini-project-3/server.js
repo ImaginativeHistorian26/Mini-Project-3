fa88181a0e056422b8ff2edcef3100cc51683125
Your Name <you@example.com>
2026-07-02 19:27:58 -0500

    Added boiler plate code for server.js to initialize server at PORT 3000

---
+++ mini-project-3/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+//require("dotenv").config();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "mini-project-3"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
