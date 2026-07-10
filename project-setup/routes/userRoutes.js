3c56df17916f83e03f9aa12d4929cde24d86ff02
Your Name <you@example.com>
2026-07-10 00:18:22 -0500

    Added createUSER for const; included getUser and createUser for routers

--- project-setup/routes/userRoutes.js
+++ project-setup/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getUser, createUser } = require("../controllers/userController");
 
-router.get("/", (req, res) => {
-    res.send("Test");
-});
+router.get("/", getUser);
+router.post("/create", createUser);
 
 module.exports = router;
