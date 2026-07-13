9d570db5fc33d1457c99ec6887cbafe3abafede8
Your Name <you@example.com>
2026-07-13 17:55:06 -0500

    Added const for updateUser and deleteUser; added updateUser and deleteUser to module.exports

--- project-setup/controllers/userController.js
+++ project-setup/controllers/userController.js
@@ -19,6 +19,28 @@ const createUser = (req, res) => {
     });
 };
 
+const updateUser = (req, res) => {
+  console.log(req.body);
+  Models.User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const deleteUser = (req, res) => {
+  Models.User.findByIdAndDelete(req.params.id)
+    .then((data) => res.send({ result: 200, data: data }))
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
 module.exports = {
-  getUser, createUser
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
 };
