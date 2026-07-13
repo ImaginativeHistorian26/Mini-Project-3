75127985ced5e7987d3cb728b06d723e84d89156
Your Name <you@example.com>
2026-07-13 17:56:06 -0500

    Added put and delete in a router

--- project-setup/routes/userRoutes.js
+++ project-setup/routes/userRoutes.js
@@ -4,5 +4,12 @@ const { getUser, createUser } = require("../controllers/userController");
 
 router.get("/", getUser);
 router.post("/create", createUser);
+router.put("/:id", (req, res) => {
+  Controllers.userController.updateUser(req, res);
+});
+
+router.delete("/:id", (req, res) => {
+  Controllers.userController.deleteUser(req, res);
+});
 
 module.exports = router;
