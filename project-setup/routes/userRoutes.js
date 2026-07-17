8648cd12a8fec28429a932327b42db23541a5021
Your Name <you@example.com>
2026-07-17 15:55:48 -0500

    Added updateUser and deleteUser to const and shortened them in router.put and router.delete

--- project-setup/routes/userRoutes.js
+++ project-setup/routes/userRoutes.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getUser, createUser } = require("../controllers/userController");
+const { getUser, createUser, updateUser, deleteUser } = require("../controllers/userController");
 
 router.get("/", getUser);
 router.post("/create", createUser);
-router.put("/:id", (req, res) => {
-  Controllers.userController.updateUser(req, res);
-});
-router.delete("/:id", (req, res) => {
-  Controllers.userController.deleteUser(req, res);
-});
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
