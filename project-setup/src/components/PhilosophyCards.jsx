6d133c781c11d9735b56ac12c8a7e5b8dc9cfe9e
Your Name <you@example.com>
2026-07-17 15:56:58 -0500

    Added fetch request with a JSON placeholder in the useEffect section of the function PhilosophyCards()

--- project-setup/src/components/PhilosophyCards.jsx
+++ project-setup/src/components/PhilosophyCards.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
 const famousPhilosophers = [
-  { image: "../public/Plato.png", title: 'Platonism' },
-  { image: "../public/Aristotle.png", title: 'Aristotelianism' },
-  { image: "../public/Plotinus.png", title: 'Neo-Platonism' },
-  { image: "../public/Aquinas.png", title: 'Scholasticism' },
-  { image: "../public/Kant.png", title: 'Kantianism' },
-  { image: "../public/Kierkegaard.png", title: 'Existentialism' },
+  { image: "../public/Plato.png", title: "Platonism" },
+  { image: "../public/Aristotle.png", title: "Aristotelianism" },
+  { image: "../public/Plotinus.png", title: "Neo-Platonism" },
+  { image: "../public/Aquinas.png", title: "Scholasticism" },
+  { image: "../public/Kant.png", title: "Kantianism" },
+  { image: "../public/Kierkegaard.png", title: "Existentialism" },
 ];
 
 function PhilosophyCards() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <div className="container">
       <div id="cards-container" className="row justify-content-center g-4">
@@ -17,12 +22,19 @@ function PhilosophyCards() {
           <div key={i} className="col-12 col-md-6 col-lg-4">
             <div className="card h-100 shadow-sm text-center">
               <div className="p-3">
-                <img src={p.image} className="card-img-top img-fluid" alt={p.title} />
+                <img
+                  src={p.image}
+                  className="card-img-top img-fluid"
+                  alt={p.title}
+                />
               </div>
               <div className="card-body d-flex flex-column justify-content-between">
                 <h3 className="card-title my-3">{p.title}</h3>
+                <p>{}</p>
                 <p className="card-text">{p.text}</p>
-                <a href="#" className="btn btn-primary w-100 mx-auto mt-auto">Check it out!</a>
+                <a href="#" className="btn btn-primary w-100 mx-auto mt-auto">
+                  Check it out!
+                </a>
               </div>
             </div>
           </div>
