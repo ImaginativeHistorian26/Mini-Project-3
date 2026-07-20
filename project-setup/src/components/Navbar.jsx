1937588dc18e40bfbe95b952c24c41a0c240561d
Your Name <you@example.com>
2026-07-20 15:59:22 -0500

    Changed 'class' to 'className'

--- project-setup/src/components/Navbar.jsx
+++ project-setup/src/components/Navbar.jsx
@@ -2,24 +2,24 @@ import { Link } from "react-router";
 
 function Navbar() {
   return (
-    <div class="header-nav-con">
+    <div className="header-nav-con">
       <header>
-        <h1 class="display-2 text-center">The Academy of Thinkers</h1>
+        <h1 className="display-2 text-center">The Academy of Thinkers</h1>
       </header>
 
-      <div class="container-fluid px-0">
-        <nav class="navbar navbar-expand navbar-light d-flex justify-content-end">
-          <ul class="nav-list-custom mb-0">
-            <li class="navbtn">
+      <div className="container-fluid px-0">
+        <nav className="navbar navbar-expand navbar-light d-flex justify-content-end">
+          <ul className="nav-list-custom mb-0">
+            <li className="navbtn">
               <Link to="/"> Home </Link>
             </li>
-            <li class="navbtn">
+            <li className="navbtn">
               <Link to="/signup">Sign-Up</Link>
             </li>
-            <li class="navbtn">
+            <li className="navbtn">
               <Link to="/topics">Topics</Link>
             </li>
-            <li class="navbtn">
+            <li className="navbtn">
               <Link to="/about">About</Link>
             </li>
           </ul>
