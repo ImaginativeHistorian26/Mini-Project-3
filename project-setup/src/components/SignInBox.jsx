324ba86858c69648ac5f8c020d6912f550ee998a
Your Name <you@example.com>
2026-07-20 16:08:51 -0500

    Handled e.preventDefault in function; revised inputs with e.target.value

--- project-setup/src/components/SignInBox.jsx
+++ project-setup/src/components/SignInBox.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function SignInBox() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ username, password, email });
+  };
+
   return (
     <div className="container my-4">
       <article className="sign-up-form">
-
         <div className="sign-up-container container mx-auto">
           <h2>Sign up or login to comment on posts.</h2>
-          
-          <form action="/action_page.php" method="post">
+          <form onSubmit={handleSubmit}>
             <div className="sign-up-container-2">
-              <input type="text" placeholder="Username" name="uname" required />
-
-              <input type="password" placeholder="Password" name="psw" required />
-
-              <input type="email" placeholder="Email" name="eml" required />
-
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
               <button className="form-btn" type="submit">
                 Sign Up
               </button>
@@ -24,7 +46,6 @@ function SignInBox() {
               </button>
             </div>
           </form>
-          
         </div>
       </article>
     </div>
