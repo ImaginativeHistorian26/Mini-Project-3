4a5d55485e3e16458d3c5dce53c95eb39afbda09
Your Name <you@example.com>
2026-07-20 00:26:35 -0500

    Implemented comments for @openapi for getUser; added getUserById for userController const; added router.get for getUserById

--- project-setup/routes/userRoutes.js
+++ project-setup/routes/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { getUser, createUser, updateUser, deleteUser } = require("../controllers/userController");
+const { getUser, createUser, getUserById, updateUser, deleteUser } = require("../controllers/userController");
 
+/**
+ * @openapi
+ * /users:
+ *   get:
+ *     summary: List users
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 router.get("/", getUser);
 router.post("/create", createUser);
+router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
